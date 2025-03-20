45654750f767014a9b712817684b2284fd28d00d
Maarten Ceulemans <r1048459@student.thomasmore.be>
2025-03-20 20:17:49 +0100

    Update IndividualProject.py

--- IndividualProject/IndividualProject.py
+++ IndividualProject/IndividualProject.py
@@ -1,4 +1,4 @@
-import OPi.GPIO as GPIO
+import wiringpi
 import smbus2
 import time
 import paho.mqtt.client as mqtt
@@ -21,12 +21,12 @@ MQTT_PASSWORD = "0c/35VFsF8CJUw3nqMlaSCPv"
 MQTT_CLIENT_ID = "EAEWESgCDS8kPTAYFjkLERk"
 MQTT_TOPIC = "channels/2884475/publish"
 
-# Initialisatie van GPIO
-GPIO.setmode(GPIO.BOARD)          # Gebruik fysieke pin nummering
+# Initialisatie van WiringPi
+wiringpi.wiringPiSetup()          # Gebruik WiringPi pin nummering
 for pin in STEPPER_PINS:
-    GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(LED_PIN, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(SWITCH_PIN, GPIO.IN)   # Geen pull-up weerstand
+    wiringpi.pinMode(pin, wiringpi.OUTPUT)
+wiringpi.pinMode(LED_PIN, wiringpi.OUTPUT)
+wiringpi.pinMode(SWITCH_PIN, wiringpi.INPUT)
 
 # Initialisatie van I2C
 bus = smbus2.SMBus(I2C_BUS)
@@ -42,9 +42,10 @@ STEP_SEQUENCE = [
 # Functie om de stepper motor te draaien
 def step_motor(steps, direction=1):
     for _ in range(steps):
-        for step in STEP_SEQUENCE[::direction]:
-            for pin, value in zip(STEPPER_PINS, step):
-                GPIO.write(pin, value)
+        for i in range(len(STEP_SEQUENCE)):
+            step = STEP_SEQUENCE[i if direction == 1 else len(STEP_SEQUENCE) - 1 - i]
+            for j in range(len(STEPPER_PINS)):
+                wiringpi.digitalWrite(STEPPER_PINS[j], step[j])
             time.sleep(0.01)
 
 # Functie om de stepper motor te resetten
@@ -57,17 +58,21 @@ def read_bh1750():
     bytes_read = bus.read_i2c_block_data(BH1750_ADDR, 0x10, 2)
     return ((bytes_read[0] << 8) + bytes_read[1]) / 1.2
 
-# Functie om de temperatuur te lezen van de BMP280
+# Functie om de temperatuur en druk te lezen van de BMP280
 def read_bmp280():
     # BMP280 uitlezen zonder calibration_params
     data = bus.read_i2c_block_data(BMP280_ADDR, 0xFA, 6)
     temp_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4)
     temp = (temp_raw / 16 - 27315) / 100  # Omrekenen naar graden Celsius
-    return temp
+
+    # Druk uitlezen
+    press_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4)
+    press = press_raw / 100  # Omrekenen naar hPa
+    return temp, press
 
 # Functie om data naar ThingSpeak te sturen via MQTT
-def send_to_thingspeak(temp, lux):
-    payload = f"field1={temp}&field2={lux}"
+def send_to_thingspeak(temp, lux, press):
+    payload = f"field1={temp}&field2={lux}&field3={press}"
     try:
         client.publish(MQTT_TOPIC, payload)
         print(f"ThingSpeak Data Sent: {payload}")
@@ -84,10 +89,10 @@ try:
     while True:
         # Lees de sensoren
         lux = read_bh1750()
-        temp = read_bmp280()
+        temp, press = read_bmp280()
 
         # Publiceer de sensorwaarden naar ThingSpeak
-        send_to_thingspeak(temp, lux)
+        send_to_thingspeak(temp, lux, press)
 
         # Controleer de lux-waarde en temperatuur
         if lux > 22:  # Als de lux-waarde hoger is dan 22
@@ -96,11 +101,11 @@ try:
             reset_motor()  # Reset de stepper motor
 
         # Controleer de switch
-        if GPIO.read(SWITCH_PIN) == GPIO.LOW:  # Als de switch is ingedrukt
-            GPIO.write(LED_PIN, GPIO.HIGH)  # Zet de LED aan
+        if wiringpi.digitalRead(SWITCH_PIN) == wiringpi.LOW:  # Als de switch is ingedrukt
+            wiringpi.digitalWrite(LED_PIN, wiringpi.HIGH)  # Zet de LED aan
             step_motor(50)  # Draai de stepper motor
         else:  # Als de switch niet is ingedrukt
-            GPIO.write(LED_PIN, GPIO.LOW)  # Zet de LED uit
+            wiringpi.digitalWrite(LED_PIN, wiringpi.LOW)  # Zet de LED uit
             reset_motor()  # Reset de stepper motor
 
         # Wacht even voordat de volgende meting
@@ -108,6 +113,8 @@ try:
 
 except KeyboardInterrupt:
     # Opruimen bij het afsluiten van het script
-    GPIO.cleanup()
+    for pin in STEPPER_PINS:
+        wiringpi.digitalWrite(pin, wiringpi.LOW)
+    wiringpi.digitalWrite(LED_PIN, wiringpi.LOW)
     client.disconnect()
     print("Script gestopt.")
